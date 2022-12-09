a894ecdccb6d7f6f21ed168cdb395cff5cb84461
Yamcz00r <99492855+Yamcz00r@users.noreply.github.com>
2022-12-09 23:34:35 +0100

    create_login_ui

---
+++ src/components/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCZo-Q_jRAFeO-KPHfzIxu8CmUmvyJjjLA",
+  authDomain: "auth-system-ff45a.firebaseapp.com",
+  projectId: "auth-system-ff45a",
+  storageBucket: "auth-system-ff45a.appspot.com",
+  messagingSenderId: "830694590447",
+  appId: "1:830694590447:web:dc0e0b1eeef2f49f1d981a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
