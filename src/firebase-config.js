43ba9283c75891a93b00273e314420bdaa7a218e
Yamcz00r <99492855+Yamcz00r@users.noreply.github.com>
2023-02-02 11:31:48 +0100

    added-env-variables

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCZo-Q_jRAFeO-KPHfzIxu8CmUmvyJjjLA",
-  authDomain: "auth-system-ff45a.firebaseapp.com",
-  projectId: "auth-system-ff45a",
-  storageBucket: "auth-system-ff45a.appspot.com",
-  messagingSenderId: "830694590447",
-  appId: "1:830694590447:web:dc0e0b1eeef2f49f1d981a"
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID
 };
 
 // Initialize Firebase
